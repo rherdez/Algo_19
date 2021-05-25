4c9bfd9824552e3d6464cc4072461f101373d4f3
Roberto <roberto.herna@gmail.com>
2021-05-24 19:38:54 -0600

    $m

---
+++ I/App1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	cout<<"Hola mundo"<<endl;
+	return 0;
+}
