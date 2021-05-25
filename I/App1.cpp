83d6a3f3a3ac764984f1dd3e46421cb374a03e88
Roberto <roberto.herna@gmail.com>
2021-05-24 19:58:11 -0600

    $m

--- I/App1.cpp
+++ I/App1.cpp
@@ -3,6 +3,27 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 int main(int argc, char** argv) {
-	cout<<"Hola mundo"<<endl;
+	int x;
+	bool c=true;
+	
+/*	cout<<"Ingrese un valor: "<<endl;
+	cin>>x;
+	cout<<"El valor: "<<x<<"\n";
+	
+	<
+	>
+	<=
+	>=
+	!=
+	
+	*/
+	if(x=5){
+		cout<<"Usted es Ciudadano"<<endl;
+	}
+	else{
+		cout<<"Es un niño"<<endl;
+	}
+		
+	
 	return 0;
 }
